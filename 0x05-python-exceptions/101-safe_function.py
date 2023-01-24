cfc29d60f30887e113b2e280ee1e05e0ba30b7f1
Shihabuddeen <shihabuddeenj@gmail.com>
2023-01-24 04:02:43 -0800

    hot exception

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
 
-
 import sys
-
-
 
 def safe_function(fct, *args):
 
     """Executes a function safely.
-
 
     Args:
 
         fct: The function to execute.
 
         args: Arguments for fct.
-
 
     Returns:
 
